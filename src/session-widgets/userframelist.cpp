ef68528c6cb1d2c16a292a448d6221f6aa0d536e
lixin <920290760@qq.com>
2019-08-20 14:48:09 +0800

    feat: add class UserFrameList
    V20 login and lock UserFrame

---
+++ src/session-widgets/userframelist.cpp
@@ -0,0 +1,125 @@
+/*
+ * Copyright (C) 2019 ~ 2019 Deepin Technology Co., Ltd.
+ *
+ * Author:     lixin <lixin_cm@deepin.com>
+ *
+ * Maintainer: lixin <lixin_cm@deepin.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "userframelist.h"
+#include "sessionbasemodel.h"
+#include "userinfo.h"
+#include "userloginwidget.h"
+
+#include "dflowlayout.h"
+
+#include <QVBoxLayout>
+#include <QScrollArea>
+
+const int UserFrameHeight = 150;
+const int UserFrameWidth = 200;
+const int UserFrameSpaceing = 30;
+const int UserAvatarSize = 50;
+const int UserFrameRowCount = 2;
+const int UserFrameColCount = 5;
+
+UserFrameList::UserFrameList(QWidget *parent)
+    : QWidget(parent)
+    , m_scrollArea(new QScrollArea(this))
+{
+    initUI();
+}
+
+//设置SessionBaseModel，创建用户列表窗体
+void UserFrameList::setModel(SessionBaseModel *model)
+{
+    m_model = model;
+
+    connect(model, &SessionBaseModel::onUserAdded, this, &UserFrameList::addUser);
+    connect(model, &SessionBaseModel::onUserRemoved, this, &UserFrameList::removeUser);
+
+    QList<std::shared_ptr<User>> userList = m_model->userList();
+    for (auto user : userList) {
+        addUser(user);
+    }
+}
+
+//创建用户窗体
+void UserFrameList::addUser(std::shared_ptr<User> user)
+{
+    UserLoginWidget *widget = new UserLoginWidget(this);
+    widget->setFixedSize(QSize(UserFrameWidth, UserFrameHeight));
+    widget->setUserAvatarSize(UserAvatarSize, UserAvatarSize);
+    widget->setAvatar(user->avatarPath());
+    widget->setName(user->name());
+
+    m_userLoginWidgets[user->uid()] = widget;
+    m_folwLayout->addWidget(widget);
+}
+
+//删除用户
+void UserFrameList::removeUser(const uint uid)
+{
+    UserLoginWidget *widget = m_userLoginWidgets[uid];
+    m_userLoginWidgets.remove(uid);
+    widget->deleteLater();
+}
+
+//点击用户
+void UserFrameList::onUserClicked()
+{
+    UserLoginWidget *widget = static_cast<UserLoginWidget *>(sender());
+    if (!widget) return;
+
+    emit requestSwitchUser(m_model->findUserByUid(m_userLoginWidgets.key(widget))); \
+}
+
+void UserFrameList::resizeEvent(QResizeEvent *event)
+{
+    Q_UNUSED(event);
+    //处理窗体数量小于5个时的居中显示，取 窗体数量*窗体宽度 和 最大宽度 的较小值，设置为m_centerWidget的宽度
+    int width = qMin((UserFrameWidth + UserFrameSpaceing) * UserFrameColCount, m_userLoginWidgets.size() * (UserFrameWidth + UserFrameSpaceing));
+    m_centerWidget->setFixedWidth(width);
+    m_scrollArea->setFixedWidth(width + 10);
+}
+
+void UserFrameList::initUI()
+{
+    setFocusPolicy(Qt::NoFocus);
+    m_folwLayout = new DFlowLayout;
+    m_folwLayout->setFlow(QListView::LeftToRight);
+    m_folwLayout->setMargin(0);
+    m_folwLayout->setSpacing(UserFrameSpaceing);
+
+    m_centerWidget = new QWidget;
+    m_centerWidget->setLayout(m_folwLayout);
+    //m_centerWidget设置固定宽度，实现m_folwLayout布局插入UserLoginWidget时，一行最多插入5个
+    m_centerWidget->setFixedWidth((UserFrameWidth + UserFrameSpaceing) * UserFrameColCount);
+
+    m_scrollArea->setWidgetResizable(true);
+    m_scrollArea->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+    //m_scrollArea纵向滑动条在需要时显示，横向不显示
+    m_scrollArea->setVerticalScrollBarPolicy(Qt::ScrollBarAsNeeded);
+    m_scrollArea->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    m_scrollArea->setWidget(m_centerWidget);
+    //设置m_scrollArea固定宽度为两行窗体高，控制滑动条的显示
+    m_scrollArea->setFixedHeight((UserFrameHeight + UserFrameSpaceing) * UserFrameRowCount);
+
+    QVBoxLayout *mainLayout;
+    mainLayout = new QVBoxLayout;
+    mainLayout->addWidget(m_scrollArea, 0, Qt::AlignCenter);
+    setLayout(mainLayout);
+}
---
+++ src/session-widgets/userframelist.h
@@ -0,0 +1,67 @@
+/*
+ * Copyright (C) 2019 ~ 2019 Deepin Technology Co., Ltd.
+ *
+ * Author:     lixin <lixin_cm@deepin.com>
+ *
+ * Maintainer: lixin <lixin_cm@deepin.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef USERFRAMELIST_H
+#define USERFRAMELIST_H
+
+#include <dflowlayout.h>
+
+#include <QMap>
+
+#include <memory>
+
+class User;
+class SessionBaseModel;
+class UserLoginWidget;
+class QVBoxLayout;
+class QScrollArea;
+
+DWIDGET_USE_NAMESPACE
+
+class UserFrameList : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit UserFrameList(QWidget *parent = nullptr);
+    void setModel(SessionBaseModel *model);
+
+signals:
+    void requestSwitchUser(std::shared_ptr<User> user);
+    void hideFrame();
+
+protected:
+    void resizeEvent(QResizeEvent *event) override;
+
+private:
+    void initUI();
+    void addUser(std::shared_ptr<User> user);
+    void removeUser(const uint uid);
+    void onUserClicked();
+
+private:
+    QScrollArea *m_scrollArea;
+    DFlowLayout *m_folwLayout;
+    QMap<uint, UserLoginWidget *> m_userLoginWidgets;
+    SessionBaseModel *m_model;
+    QWidget *m_centerWidget;
+};
+
+#endif // USERFRAMELIST_H
