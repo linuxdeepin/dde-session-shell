207627aa3880522122382315a42ee27f6873c168
Hualet Wang <mr.asianwang@gmail.com>
2019-09-30 11:06:12 +0800

    chore: setup translations

---
+++ translations/dde-session-shell_en_US.ts
@@ -0,0 +1,235 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>ContentWidget</name>
+    <message>
+        <location filename="../src/dde-shutdown/view/contentwidget.cpp" line="315"/>
+        <source>The programs are preventing the computer from shutting down / hibernation, and forcing shut down / hibernate may cause data loss.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/dde-shutdown/view/contentwidget.cpp" line="316"/>
+        <source>To close the program, Click Cancel, and then close the program.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/dde-shutdown/view/contentwidget.cpp" line="328"/>
+        <location filename="../src/dde-shutdown/view/contentwidget.cpp" line="365"/>
+        <location filename="../src/dde-shutdown/view/contentwidget.cpp" line="387"/>
+        <location filename="../src/dde-shutdown/view/contentwidget.cpp" line="521"/>
+        <source>Shut down</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/dde-shutdown/view/contentwidget.cpp" line="330"/>
+        <location filename="../src/dde-shutdown/view/contentwidget.cpp" line="367"/>
+        <location filename="../src/dde-shutdown/view/contentwidget.cpp" line="390"/>
+        <location filename="../src/dde-shutdown/view/contentwidget.cpp" line="524"/>
+        <source>Reboot</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/dde-shutdown/view/contentwidget.cpp" line="332"/>
+        <location filename="../src/dde-shutdown/view/contentwidget.cpp" line="527"/>
+        <source>Suspend</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/dde-shutdown/view/contentwidget.cpp" line="334"/>
+        <location filename="../src/dde-shutdown/view/contentwidget.cpp" line="530"/>
+        <source>Hibernate</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/dde-shutdown/view/contentwidget.cpp" line="336"/>
+        <location filename="../src/dde-shutdown/view/contentwidget.cpp" line="393"/>
+        <location filename="../src/dde-shutdown/view/contentwidget.cpp" line="536"/>
+        <source>Log out</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/dde-shutdown/view/contentwidget.cpp" line="388"/>
+        <source>Are you sure you want to shut down?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/dde-shutdown/view/contentwidget.cpp" line="391"/>
+        <source>Are you sure you want to reboot?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/dde-shutdown/view/contentwidget.cpp" line="394"/>
+        <source>Are you sure you want to log out?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/dde-shutdown/view/contentwidget.cpp" line="533"/>
+        <source>Lock</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/dde-shutdown/view/contentwidget.cpp" line="540"/>
+        <source>Switch user</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>GreeterWorkek</name>
+    <message>
+        <location filename="../src/lightdm-deepin-greeter/greeterworkek.cpp" line="94"/>
+        <source>Domain account</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/lightdm-deepin-greeter/greeterworkek.cpp" line="288"/>
+        <source>Wrong Password</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/lightdm-deepin-greeter/greeterworkek.cpp" line="293"/>
+        <source>The domain account or password is not correct. Please enter again.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>InhibitWarnView</name>
+    <message>
+        <location filename="../src/dde-shutdown/view/inhibitwarnview.cpp" line="88"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LockWorker</name>
+    <message>
+        <location filename="../src/dde-lock/lockworker.cpp" line="92"/>
+        <source>Domain account</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/dde-lock/lockworker.cpp" line="211"/>
+        <source>Fingerprint verification timed out, please enter your password manually</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/dde-lock/lockworker.cpp" line="229"/>
+        <source>Failed to match fingerprint</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/dde-lock/lockworker.cpp" line="264"/>
+        <source>Wrong Password</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MultiUsersWarningView</name>
+    <message>
+        <location filename="../src/dde-shutdown/view/multiuserswarningview.cpp" line="45"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/dde-shutdown/view/multiuserswarningview.cpp" line="126"/>
+        <source>The above users are still logged in and data will be lost due to shutdown, are you sure you want to shut down?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/dde-shutdown/view/multiuserswarningview.cpp" line="130"/>
+        <source>The above users are still logged in and data will be lost due to reboot, are you sure you want to reboot?</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ShutdownWidget</name>
+    <message>
+        <location filename="../src/session-widgets/userinputwidget.cpp" line="285"/>
+        <location filename="../src/widgets/shutdownwidget.cpp" line="29"/>
+        <source>Shut down</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/session-widgets/userinputwidget.cpp" line="301"/>
+        <source>Login</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/session-widgets/userinputwidget.cpp" line="310"/>
+        <location filename="../src/widgets/shutdownwidget.cpp" line="30"/>
+        <location filename="../src/widgets/shutdownwidget.cpp" line="101"/>
+        <source>Reboot</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/widgets/shutdownwidget.cpp" line="31"/>
+        <location filename="../src/widgets/shutdownwidget.cpp" line="108"/>
+        <source>Suspend</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/widgets/shutdownwidget.cpp" line="32"/>
+        <location filename="../src/widgets/shutdownwidget.cpp" line="115"/>
+        <source>Hibernate</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SystemMonitor</name>
+    <message>
+        <location filename="../src/dde-shutdown/view/systemmonitor.cpp" line="45"/>
+        <source>Start system monitor</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>TimeWidget</name>
+    <message>
+        <location filename="../src/widgets/timewidget.cpp" line="82"/>
+        <source>hh:mm</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/widgets/timewidget.cpp" line="84"/>
+        <source>hh:mm ap</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/widgets/timewidget.cpp" line="87"/>
+        <source>yyyy-MM-dd dddd</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>UserInputWidget</name>
+    <message>
+        <location filename="../src/session-widgets/userinputwidget.cpp" line="13"/>
+        <source>Login</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../src/session-widgets/userinputwidget.cpp" line="257"/>
+        <source>Please try again %n minute(s) later</source>
+        <translation type="unfinished">
+            <numerusform></numerusform>
+            <numerusform></numerusform>
+        </translation>
+    </message>
+</context>
+<context>
+    <name>UserLoginWidget</name>
+    <message numerus="yes">
+        <location filename="../src/session-widgets/userloginwidget.cpp" line="235"/>
+        <source>Please try again %n minute(s) later</source>
+        <translation type="unfinished">
+            <numerusform></numerusform>
+            <numerusform></numerusform>
+        </translation>
+    </message>
+    <message>
+        <location filename="../src/session-widgets/userloginwidget.cpp" line="395"/>
+        <source>UserLoginPassword</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
