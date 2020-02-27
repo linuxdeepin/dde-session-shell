0cad9c05f371b102dbc25086094828f12ec8482c
zorowk <near.kingzero@gmail.com>
2020-02-27 17:19:16 +0800

    fix: unlock icon error after hibernation
    - 休眠解除后解锁图标错误 BUG 13280

--- src/dde-lock/lockframe.cpp
+++ src/dde-lock/lockframe.cpp
@@ -52,7 +52,7 @@ LockFrame::LockFrame(SessionBaseModel *const model, QWidget *parent)
     connect(model, &SessionBaseModel::showUserList, this, &LockFrame::showUserList);
     connect(model, &SessionBaseModel::authFinished, this, [ = ](bool success) {
         if (success) {
-            m_content->beforeUnlockAction();
+            //m_content->beforeUnlockAction();
             Q_EMIT requestEnableHotzone(true);
             hide();
         }
