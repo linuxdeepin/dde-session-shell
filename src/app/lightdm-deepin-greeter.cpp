71c03059bf7c03a4ae6753ac79b90ed5e1289d0a
yexin <1072390773@qq.com>
2020-07-16 15:46:43 +0800

    feat(log): session shell各模块记录崩溃时间和次数
    session shell各模块记录崩溃时间和次数
    
    Log: session shell各模块记录崩溃时间和次数
    Task: https://pms.uniontech.com/zentao/task-view-28507.html

--- src/app/lightdm-deepin-greeter.cpp
+++ src/app/lightdm-deepin-greeter.cpp
@@ -38,11 +38,20 @@
 #include <QSettings>
 #include <QWindow>
 #include <QScreen>
+#include <QStandardPaths>
 #include <DLog>
 #include <QDesktopWidget>
+
 #include <DGuiApplicationHelper>
 
 #include <cstdlib>
+#include <string>
+
+#include <stdio.h>
+#include <time.h>
+#include <execinfo.h>
+#include <sys/stat.h>
+#include <signal.h>
 
 #include <X11/Xlib.h>
 #include <X11/extensions/Xrandr.h>
@@ -53,6 +62,87 @@
 
 DCORE_USE_NAMESPACE
 DWIDGET_USE_NAMESPACE
+
+const int MAX_STACK_FRAMES = 128;
+
+using namespace std;
+
+void sig_crash(int sig)
+{
+    FILE *fd;
+    struct stat buf;
+    char path[100];
+    memset(path, 0, 100);
+    //崩溃日志路径
+    QString strPath = QStandardPaths::standardLocations(QStandardPaths::ConfigLocation)[0] + "/dde-collapse.log";
+    memcpy(path, strPath.toStdString().data(), strPath.length());
+
+    stat(path, &buf);
+    if (buf.st_size > 10 * 1024 * 1024) {
+        // 超过10兆则清空内容
+        fd = fopen(path, "w");
+    } else {
+        fd = fopen(path, "at");
+    }
+
+    if (nullptr == fd) {
+        exit(0);
+    }
+    //捕获异常，打印崩溃日志到配置文件中
+    try {
+        char szLine[512] = {0};
+        time_t t = time(nullptr);
+        tm *now = localtime(&t);
+        QString log = "#####" + qApp->applicationName() + "#####\n[%04d-%02d-%02d %02d:%02d:%02d][crash signal number:%d]\n";
+        int nLen1 = sprintf(szLine, log.toStdString().c_str(),
+                            now->tm_year + 1900,
+                            now->tm_mon + 1,
+                            now->tm_mday,
+                            now->tm_hour,
+                            now->tm_min,
+                            now->tm_sec,
+                            sig);
+        fwrite(szLine, 1, strlen(szLine), fd);
+
+#ifdef __linux
+        void *array[MAX_STACK_FRAMES];
+        size_t size = 0;
+        char **strings = nullptr;
+        size_t i, j;
+        signal(sig, SIG_DFL);
+        size = backtrace(array, MAX_STACK_FRAMES);
+        strings = (char **)backtrace_symbols(array, size);
+        for (i = 0; i < size; ++i) {
+            char szLine[512] = {0};
+            sprintf(szLine, "%d %s\n", i, strings[i]);
+            fwrite(szLine, 1, strlen(szLine), fd);
+
+            std::string symbol(strings[i]);
+
+            size_t pos1 = symbol.find_first_of("[");
+            size_t pos2 = symbol.find_last_of("]");
+            std::string address = symbol.substr(pos1 + 1, pos2 - pos1 - 1);
+            char cmd[128] = {0};
+            sprintf(cmd, "addr2line -C -f -e lightdm-deepin-greeter %s", address.c_str()); // 打印当前进程的id和地址
+            FILE *fPipe = popen(cmd, "r");
+            if (fPipe != nullptr) {
+                char buff[1024];
+                memset(buff, 0, sizeof(buff));
+                char *ret = fgets(buff, sizeof(buff), fPipe);
+                pclose(fPipe);
+                fwrite(ret, 1, strlen(ret), fd);
+            }
+        }
+        free(strings);
+#endif // __linux
+    } catch (...) {
+        //
+    }
+    fflush(fd);
+    fclose(fd);
+    fd = nullptr;
+    exit(0);
+}
 
 //Load the System cursor --begin
 static XcursorImages*
@@ -196,6 +286,14 @@ int main(int argc, char* argv[])
     qApp->setApplicationName("lightdm-deepin-greeter");
     qApp->setApplicationVersion("2015.1.0");
     qApp->setAttribute(Qt::AA_ForceRasterWidgets);
+
+    //崩溃信号
+    signal(SIGTERM, sig_crash);
+    signal(SIGSEGV, sig_crash);
+    signal(SIGILL, sig_crash);
+    signal(SIGINT, sig_crash);
+    signal(SIGABRT, sig_crash);
+    signal(SIGFPE, sig_crash);
 
     DGuiApplicationHelper::instance()->setPaletteType(DGuiApplicationHelper::LightType);
     DPalette pa = DGuiApplicationHelper::instance()->applicationPalette();
