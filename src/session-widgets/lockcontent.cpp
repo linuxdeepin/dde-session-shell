0877769facd977b5be1f34467ba8630e28ebf739
xuyanghe <xuyanghe@uniontech.com>
2020-09-13 13:44:37 +0800

    fix: UserA login-widget flash first time changing user
    mips64el系统在第一次点击电源-切用户的时候，会闪现原用户登陆界面。因为之前的releaseMouse里重复调用了一次登陆界面
    
    Log: 切换用户时闪出原用户的登录界面，再切换到新用户登录界面
    Bug: https://pms.uniontech.com/zentao/bug-view-43299.html
    
    Change-Id: I2abbdb7d15ce32cb387eb7133faa2d143b0cf4c7
    Reviewed-on: http://gerrit.uniontech.com/c/dde-session-shell/+/4100
    Reviewed-by: <mailman@uniontech.com>
    Reviewed-by: pengwenhao <pengwenhao@uniontech.com>
    Reviewed-by: lizhongming <lizhongming@uniontech.com>
    Tested-by: <mailman@uniontech.com>
    Reviewed-on: http://gerrit.uniontech.com/c/dde-session-shell/+/5120

--- src/session-widgets/lockcontent.cpp
+++ src/session-widgets/lockcontent.cpp
@@ -31,7 +31,7 @@ LockContent::LockContent(SessionBaseModel *const model, QWidget *parent)
     m_mediaWidget = nullptr;
 
     m_shutdownFrame->setModel(model);
-    m_model->setCurrentModeState(SessionBaseModel::ModeStatus::PasswordMode);
+    restoreCenterContent();
 
     setCenterTopWidget(m_timeWidget);
     setLeftBottomWidget(m_logoWidget);
@@ -64,7 +64,7 @@ LockContent::LockContent(SessionBaseModel *const model, QWidget *parent)
     //lixin
     //connect(m_userLoginInfo, &UserLoginInfo::requestAuthUser, this, &LockContent::restoreMode);
     connect(m_userLoginInfo, &UserLoginInfo::requestAuthUser, this, &LockContent::requestAuthUser);
-    connect(m_userLoginInfo, &UserLoginInfo::hideUserFrameList, this, &LockContent::restoreMode);
+    connect(m_userLoginInfo, &UserLoginInfo::hideUserFrameList, this, &LockContent::restoreCenterContent);
     connect(m_userLoginInfo, &UserLoginInfo::requestSwitchUser, this, &LockContent::requestSwitchToUser);
     connect(m_userLoginInfo, &UserLoginInfo::requestSetLayout, this, &LockContent::requestSetLayout);
     connect(m_userLoginInfo, &UserLoginInfo::changePasswordFinished, this, &LockContent::restoreMode);
@@ -100,7 +100,6 @@ LockContent::LockContent(SessionBaseModel *const model, QWidget *parent)
     connect(model, &SessionBaseModel::onUserListChanged, this, &LockContent::onUserListChanged);
     connect(model, &SessionBaseModel::userListLoginedChanged, this, &LockContent::onUserListChanged);
     connect(model, &SessionBaseModel::authFinished, this, &LockContent::restoreMode);
-    connect(model, &SessionBaseModel::switchUserFinished, this, &LockContent::restoreMode);
     connect(m_imageBlurInter, &ImageBlur::BlurDone, this, &LockContent::onBlurDone);
 
     QTimer::singleShot(0, this, [ = ] {
@@ -221,9 +220,6 @@ void LockContent::mouseReleaseEvent(QMouseEvent *event)
 {
     if (m_model->currentModeState() == SessionBaseModel::ModeStatus::ConfirmPasswordMode)
         m_model->setAbortConfirm(false);
-    else {
-        restoreCenterContent();
-    }
 
     // hide keyboardlayout widget
     m_userLoginInfo->hideKBLayout();
