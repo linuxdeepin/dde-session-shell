9efe78892cc89e6fe88fbb06e7cfc122ed02b555
yangyuyin <yangyuyin@uniontech.com>
2021-01-15 16:34:56 +0800

    fix: debug下不限制执行lightdm-deepin-greeter
    添加QT_DEBUG
    
    Log: 修改代码不影响debug调试greeter
    Change-Id: Ib8f7026afca449ce16722d71dc24416a9ccae2d7

--- src/app/lightdm-deepin-greeter.cpp
+++ src/app/lightdm-deepin-greeter.cpp
@@ -226,8 +226,12 @@ int main(int argc, char* argv[])
 
     SessionBaseModel *model = new SessionBaseModel(SessionBaseModel::AuthType::LightdmType);
     GreeterWorkek *worker = new GreeterWorkek(model); //
+
+#ifndef QT_DEBUG
     if (!worker->isConnectSync())
         return 0;
+#endif
+
     if(model->currentUser()) {
         QTranslator translator;
         translator.load("/usr/share/dde-session-shell/translations/dde-session-shell_" + model->currentUser()->locale().split(".").first());
