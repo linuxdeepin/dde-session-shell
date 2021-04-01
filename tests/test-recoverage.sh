bae666eeca913f2fc2dcef615c6dc7017a24fd14
litao <litao@uniontech.com>
2021-04-01 20:15:40 +0800

    feat: 单元测试脚本在运行过程中不生成report问题
    单元测试脚本在运行过程中不生成report问题
    
    Log: 单元测试脚本在运行过程中不生成report问题
    Change-Id: Id53b03f7fb5f333940cbbe6897c5b7c70ce41caf

--- tests/test-recoverage.sh
+++ tests/test-recoverage.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
-
 BUILD_DIR=build
 REPORT_DIR=report
-
 cd ../
 rm -rf $BUILD_DIR
 mkdir $BUILD_DIR
 cd $BUILD_DIR
 cmake ../
 make -j 16
-make dde-lock-check
-make lightdm-deepin-greeter-check
+cd tests/
+./dde-lock/dde-lock-test
+./lightdm-deepin-greeter/lightdm-deepin-greeter-test
+lcov -c -d ./ -o coverage.info
+lcov -r coverage.info "*/tests/*" "*/usr/include*"  "*.h" "*build/*" "*/dbus/*" "*/xcb/*" -o final.info
+rm -rf ../../tests/$REPORT_DIR
+mkdir -p ../../tests/$REPORT_DIR
+genhtml -o ../../tests/$REPORT_DIR final.info
