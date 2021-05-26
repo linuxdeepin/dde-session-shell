f63b1dc639ece08de8aac275d7d8af05cd931f48
Zhang Qipeng <zhangqipeng@uniontech.com>
2021-05-26 09:54:41 +0800

    chore: 添加内存泄露检测的编译参数
    添加内存泄露检测的编译参数，输出检测文件。
    
    Log: 添加内存泄露检测的编译参数。
    Change-Id: Id035852343d85c8ef7163e914fa722d34485f8b5

--- tests/test-recoverage.sh
+++ tests/test-recoverage.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 BUILD_DIR=build
 REPORT_DIR=report
+PROJECT_REALNAME=dde-session-shell
+
 cd ../
 rm -rf $BUILD_DIR
 mkdir $BUILD_DIR
+
 cd $BUILD_DIR
+cmake -DCMAKE_BUILD_TYPE=Debug ..
 cmake ../
-make -j 16
+make -j16
+
 cd tests/
 ./lightdm-deepin-greeter/lightdm-deepin-greeter-test && ./dde-lock/dde-lock-test
+
 lcov -c -d ./ -o coverage.info
 lcov -r coverage.info "*/tests/*" "*/usr/include*"  "*.h" "*build/*" "*/dbus/*" "*/xcb/*" -o final.info
 rm -rf ../../tests/$REPORT_DIR
 mkdir -p ../../tests/$REPORT_DIR
 genhtml -o ../../tests/$REPORT_DIR final.info
+
+mv asan.log* asan_${PROJECT_REALNAME}.log
