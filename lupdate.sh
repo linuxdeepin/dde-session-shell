6c03594e8b7591366c3bc6868b96e754be392698
Zhang Qipeng <zhangqipeng@uniontech.com>
2021-06-11 14:35:46 +0800

    chore: 修改翻译配置
    * 新增参数 -locations none，用于去除翻译文件中记录的路径和行数信息。
    * 修改翻译配置项，将 en_US 替换为 en。
    
    Log: 修改翻译脚本。
    Change-Id: I3abc2414b4d90109652a82fa20759cbe6163aa00

--- lupdate.sh
+++ lupdate.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-lupdate ./ -ts -no-obsolete translations/dde-session-shell_en_US.ts
+lupdate ./ -ts -no-obsolete -locations none translations/dde-session-shell_en.ts
 #tx push -s -b m20 # Push the files that need to be translated
 #tx pull -s -b m20 # Pull the translated files
