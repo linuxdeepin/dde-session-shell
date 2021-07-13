8449978a54d399b426bd997f1cf1664adca1e030
Zhang Qipeng <zhangqipeng@uniontech.com>
2021-07-13 21:51:06 +0800

    test: 增加单元测试
    增加 LoginWindow 的单元测试。
    
    Log: 增加单元测试。
    Change-Id: I26049ec8268a85afa5bfe69a5376ec21e02207d2

--- tests/lightdm-deepin-greeter/ut_loginwindow.cpp
+++ tests/lightdm-deepin-greeter/ut_loginwindow.cpp
@@ -1,12 +1,8 @@
-#include <gtest/gtest.h>
-
-#include <QTest>
-#include <QLabel>
-#include <QSignalSpy>
-#include <QPushButton>
-
 #include "loginwindow.h"
 #include "sessionbasemodel.h"
+#include "userinfo.h"
+
+#include <gtest/gtest.h>
 
 class UT_LoginWindow : public testing::Test
 {
@@ -15,14 +11,16 @@ protected:
     void TearDown() override;
 
     SessionBaseModel *m_sessionBaseModel;
+    std::shared_ptr<User> m_user;
     LoginWindow *m_loginwindow;
 };
 
 void UT_LoginWindow::SetUp()
 {
     m_sessionBaseModel = new SessionBaseModel(SessionBaseModel::AuthType::LightdmType);
+    m_user = std::make_shared<User>();
+    m_sessionBaseModel->updateCurrentUser(m_user);
     m_loginwindow = new LoginWindow(m_sessionBaseModel);
-
 }
 
 void UT_LoginWindow::TearDown()
@@ -31,18 +29,13 @@ void UT_LoginWindow::TearDown()
     delete m_loginwindow;
 }
 
-TEST_F(UT_LoginWindow, coverage_main)
+TEST_F(UT_LoginWindow, Validity)
 {
     ASSERT_NE(m_loginwindow, nullptr);
 }
 
-TEST_F(UT_LoginWindow, coverage_isValid)
+TEST_F(UT_LoginWindow, Visibility)
 {
-
-}
-
-TEST_F(UT_LoginWindow, buttonClicked)
-{
-
-
+    m_loginwindow->show();
+    m_loginwindow->hide();
 }
