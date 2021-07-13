befaedb1a29a825a5600ed645a46f6dd42176e4b
Zhang Qipeng <zhangqipeng@uniontech.com>
2021-07-13 21:20:40 +0800

    chore: 修改单元测试执行脚本
    修改单元测试执行脚本，解决自动化测试生成报告失败的问题。
    
    Log: 修改单元测试执行脚本。
    Change-Id: Ifc0f92f7b1b1f970ac3e561929a2cfbdcdf1c44b

--- tests/test-recoverage.sh
+++ tests/test-recoverage.sh
@@ -9,17 +9,20 @@ mkdir $BUILD_DIR
 
 cd $BUILD_DIR
 cmake -DCMAKE_BUILD_TYPE=Debug ..
-cmake ../
-make -j16
+make -j$(nproc)
 
 cd tests/
-./lightdm-deepin-greeter/lightdm-deepin-greeter-test --gtest_output=xml:dde_test_report_lightdm-deepin-greeter.xml
-./dde-lock/dde-lock-test --gtest_output=xml:dde_test_report_dde-lock.xml
+lcov -c -i -d ./ -o init.info # 初始化并创建基准数据文件
 
-lcov -c -d ./ -o coverage.info
-lcov -r coverage.info "*/tests/*" "*/usr/include*"  "*.h" "*build/*" "*/dbus/*" "*/xcb/*" -o final.info
-rm -rf ../../tests/$REPORT_DIR
-mkdir -p ../../tests/$REPORT_DIR
-genhtml -o ../../tests/$REPORT_DIR final.info
+dde-lock/dde-lock-test --gtest_output=xml:dde_test_report_dde-lock.xml # 执行编译后的测试文件并生成测试用例数据报告
+mv asan.log* asan_dde-lock.log # 收集 dde-lock 内存泄漏数据
+lcov -c -d dde-lock/ -o coverage_dde-lock.info # 收集 dde-lock 单元测试数据
 
-mv asan.log* asan_${PROJECT_REALNAME}.log
+lightdm-deepin-greeter/lightdm-deepin-greeter-test --gtest_output=xml:dde_test_report_lightdm-deepin-greeter.xml # 执行编译后的测试文件并生成测试用例数据报告
+mv asan.log* asan_lightdm-deepin-greeter.log # 收集 lightdm-deepin-greeter 内存泄漏数据
+lcov -c -d lightdm-deepin-greeter/ -o coverage_lightdm-deepin-greeter.info # 收集 dde-lock 单元测试数据
+
+lcov -a init.info -a coverage_dde-lock.info -a coverage_lightdm-deepin-greeter.info -o coverage_total.info # 将单元测试数据汇总
+lcov -r coverage_total.info "*/tests/*" "*/usr/include*"  "*.h" "*build/*" "*/dbus/*" "*/xcb/*" -o final.info # 过滤不需要的信息
+
+genhtml -o $REPORT_DIR --title $PROJECT_REALNAME final.info # 将最终的单元测试覆盖率数据生成 html 文件
