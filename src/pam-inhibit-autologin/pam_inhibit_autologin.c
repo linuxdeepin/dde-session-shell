45cd52e7162c3211d10ad47902290e818200cd7e
lichangze <lichangze@uniontech.com>
2023-06-30 15:24:23 +0800

    feat: 模态更新和回滚运行场景改造
    增加中断自动更新的pam
    
    Log: 模态更新和回滚运行场景改造
    Task: https://pms.uniontech.com/task-view-274725.html
    Influence: 模态更新和回滚
    Change-Id: I3ca51c9b51d7326809e5d6ca980751bcdd9349b3

---
+++ src/pam-inhibit-autologin/pam_inhibit_autologin.c
@@ -0,0 +1,29 @@
+#include <security/pam_ext.h>
+#include <security/pam_modules.h>
+#include <security/_pam_types.h>
+#include <sys/syslog.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+// PAM 认证函数
+PAM_EXTERN int pam_sm_authenticate(pam_handle_t *pamh, int flags, int argc, const char **argv)
+{
+    // pre-greeter.sh脚本用于检查是否存在需要在greeter之前执行的程序，返回0：存在 返回非0：不存在
+    const char *shell_path = "/usr/share/dde-session-shell/greeters.d/pre-greeter";
+    FILE *file = fopen(shell_path, "r");
+    if (file != NULL) {
+        fclose(file);
+        int error_code = system(shell_path);
+        pam_syslog(pamh, LOG_INFO, "run %s return %d", shell_path, error_code);
+        // 如果脚本没有返回错误,则需要中断自动登录，跳转至greeter
+        return error_code == 0 ? PAM_PERM_DENIED : PAM_SUCCESS;
+    }
+    pam_syslog(pamh, LOG_INFO, "%s not exist, continue auto login ", shell_path);
+    return PAM_SUCCESS;
+}
+
+// PAM 设置环境函数
+PAM_EXTERN int pam_sm_setcred(pam_handle_t *pamh, int flags, int argc, const char **argv) {
+    // 在这里设置凭据（如果需要）
+    return PAM_SUCCESS;
+}
